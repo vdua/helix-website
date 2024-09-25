933a8dcbb2dee007feaf70869470b359995304c4
vdua <vdua@adobe.com>
2024-09-25 07:59:59 +0530

    using week data for month calculation as well

--- tools/rum/loader.js
+++ tools/rum/loader.js
@@ -99,8 +99,11 @@ export default class DataLoader {
     const days = 31;
     const promises = [];
     for (let i = 0; i < days; i += 1) {
-      promises.push(this.fetchUTCDay(date.toISOString()));
-      date.setDate(date.getDate() - 1);
+      for (let j = 0; j < 24; j += 1) {
+        promises.push(this.fetchUTCHour(date.toISOString()));
+        date.setTime(date.getTime() - (3600 * 1000));
+      }
+      // date.setDate(date.getDate() - 1);
     }
     const chunks = Promise.all(promises);
     return chunks;
